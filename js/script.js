4743197d55c540c192a1ab6a0c96e16d0a4d8573
Mdagoani <126675993+Mdagoani@users.noreply.github.com>
2023-05-24 16:54:46 -0300

    velha

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// Variáveis
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btn7 = 0;
+let btn8 = 0;
+let btn9 = 0;
+let controle = 0;
+let jogadas = 0;
+let finaljogo = false;
+
+// click botão 1
+function clickbtn1(){
+    let btn = document.getElementById('btn1')
+   
+    if (btn.value == "" && !finaljogo){
+        if (controle == 0){
+            controle = 1;
+            btn.value = "X"
+            btn1 = "X";
+        }
+    }
+}
